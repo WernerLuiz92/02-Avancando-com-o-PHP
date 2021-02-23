5b53aded94aad0b359403219ad99c079412affbc
Werner Luiz Gottschalt <werner@lumiun.com>
2021-02-23 08:52:50 -0300

    Arrays Associativas e Arrays Compostas

--- forEach.php
+++ forEach.php
@@ -1,24 +1,24 @@
 <?php
 
-//Arrays associativas
-    $conta1 = [
-        'titular' => 'Vinicius',
-        'saldo' => 1000
-    ];
-    $conta2 = [
-        'titular' => 'Werner',
-        'saldo' => 1250
-    ];
-    $conta3 = [
-        'titular' => 'Tiago',
-        'saldo' => 5675.20
+    $contasCorrentes = [
+        '088.013.760-65' => [
+            'titular' => 'Vinicius',
+            'saldo' => 1000
+        ], 
+        '821.971.400-00' => [
+            'titular' => 'Werner',
+            'saldo' => 1250
+        ], 
+        '745.714.030-11' => [
+            'titular' => 'Tiago',
+            'saldo' => 5675.20
+        ]
     ];
 
-
-    $contasCorrentes = [$conta1, $conta2, $conta3];
-
-    foreach ($contasCorrentes as $conta) {
+    foreach ($contasCorrentes as $cpf => $conta) {
+        echo 'CPF: '.$cpf.PHP_EOL;
         echo 'Titular: '.$conta['titular'].PHP_EOL;
         echo 'Saldo: R$ '.$conta['saldo'].PHP_EOL;
+
         echo PHP_EOL;
     }
